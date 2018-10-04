f2da2f290a2787b915dd270eeb13c4d225827205
CHOI SUJIN <amor77777777778@gmail.com>
2018-10-04 21:56:53 +0900

    ref 연습

--- hello-react-component/src/App.js
+++ hello-react-component/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import MyComponent from './MyComponent';
 import EventPractice from './EventPractice';
+import ValidationSample from './ValidationSample';
 
 class App extends Component {
   render() {
     return (
       <div>
-        {/* <MyComponent name="수진"/> */}
-        <EventPractice/>
+        <ValidationSample/>
       </div>
     );
   }
