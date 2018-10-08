faf647e2377737e0127a811a9fc4427043cd228e
CHOI SUJIN <amor77777777778@gmail.com>
2018-10-08 23:29:24 +0900

    컨포넌트 반복 예제

--- hello-react-component/src/App.js
+++ hello-react-component/src/App.js
@@ -3,13 +3,13 @@ import MyComponent from './MyComponent';
 import EventPractice from './EventPractice';
 import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ScrollBox ref={(ref) => this.scrollBox = ref}/>
-        <button onClick={this.scrollBox.scrollToBotton()}>맨밑으로</button>
+        <IterationSample/>
       </div>
     );
   }
