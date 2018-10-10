d8026a43f5d4387037d67007a761d3a4bba5a474
CHOI SUJIN <amor77777777778@gmail.com>
2018-10-10 22:37:25 +0900

    라이프사이클 메서드

--- hello-react-component/src/App.js
+++ hello-react-component/src/App.js
@@ -4,12 +4,29 @@ import EventPractice from './EventPractice';
 import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
 import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 1677215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color : getRandomColor()
+    });
+  }
+
   render() {
     return (
       <div>
-        <IterationSample/>
+        <button onClick={this.handleClick}>랜덤색성</button>
+        <LifeCycleSample color={this.state.color}/>
       </div>
     );
   }
