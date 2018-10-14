7026b77870616d70beda703ae90c534901896d7b
CHOI SUJIN <amor77777777778@gmail.com>
2018-10-14 13:21:59 +0900

    movie app 연습

--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -1,32 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import LinesEllipsis from 'react-lines-ellipsis';
 import './Movie.css';
 
 
-function Movie({title, poster}){
+function Movie({title, poster, genres, synopsis}){
     return(
-        <div>
-        <MoviePoster poster={poster}/>
-        <h1>{title}</h1>
+        <div className="Movie">
+            <div className="Movie__Colunms">
+                <MoviePoster poster={poster} alt={title}/>
+            </div>
+            <div className="Movie__Colunms">
+                <h1>{title}</h1>
+                <div className="Movie__Genres"> 
+                    {genres.map((genre, index) => <MovieGenres genre={genre} key={index} />)}
+                </div>
+                <p className="Movie__Synopsis">
+                    <LinesEllipsis
+                        text={synopsis}
+                        maxLine='3'
+                        ellipsis='...'
+                        trimRight
+                        basedOn='letters'
+                        />
+                </p>
+            </div>
         </div>
     )
 }
 
 
-function MoviePoster({poster}) {
+function MoviePoster({poster, alt}) {
     return (
-        <img src={poster}/>
+        <img src={poster} alt={alt} title={alt} className="Movie__Poster"/>
+    )
+}
+
+function MovieGenres({genre}) {
+    return (
+        <span className="Movie__Genre">{genre} </span>
     )
 }
 
 
 Movie.propTypes = {
     title : PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis :PropTypes.string.isRequired
+
 }
 
-MoviePoster.PropTypes = {
-    poster : PropTypes.string.isRequired
+MoviePoster.propTypes = {
+    poster : PropTypes.string.isRequired,
+    alt : PropTypes.string.isRequired
+}
+
+MovieGenres.propTypes = {
+    genre : PropTypes.string.isRequired
 }
 
 export default Movie;
